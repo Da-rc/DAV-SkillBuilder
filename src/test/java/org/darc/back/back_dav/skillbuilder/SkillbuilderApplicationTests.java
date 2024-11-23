e8ff89e854a95b7be296840d26b3c56bc8aa47dd
Mapacher <Davidrcabello92@gmail.com>
2024-11-23 19:58:02 +0100

    first commit

---
+++ src/test/java/org/darc/back/back_dav/skillbuilder/SkillbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.darc.back.back_dav.skillbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
